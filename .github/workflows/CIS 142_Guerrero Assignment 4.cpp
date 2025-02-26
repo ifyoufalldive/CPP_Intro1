5c7b75637473410f8e8b7eb0c266caf7b0447338
ifyoufalldive <ifyoufall.dive@gmail.com>
2025-02-26 15:32:17 -0600

    Add files via upload

---
+++ .github/workflows/CIS 142_Guerrero Assignment 4.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Francesca Guerrero
+Due Date: 10/2/2024
+Assignment 4: Write a game application that pits a user against the game to correctly guess a number between 1-100, randomly generated.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
